cbede63e400e83eb3d2a2c9a1d9b5d7b9efc8e9f
Stephen Crane <cranegames@gmail.com>
2018-11-04 12:11:39 -0800

    Added first_ge and find

--- btree_functions.h
+++ btree_functions.h
@@ -2,9 +2,24 @@
 #define BTREE_FUNCTIONS_H
 
 #include <cstdlib>
-#include "btree_node.h"
 
 using namespace std;
 
+template<typename T>
+T* f_find(T* data, const size_t s, const T& input){
+    for(size_t i = 0; i < s; i++){
+        if(data[i] == input)
+            return (data+i);
+    }
+    return nullptr;
+}
+template<typename T>
+size_t first_ge(T* data, const size_t s, const T& input){
+    for(size_t i = 0; i < s; i++){
+        if(input<=data[i])
+            return i;
+    }
+    return s;
+}
 
 #endif // BTREE_FUNCTIONS_H
